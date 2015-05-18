6d3fabffe9734e6c05be25cb1b4e7a02c7e6bc5e
Daniel Davis <daniel.davis@nih.gov>
2015-05-18 17:15:38 -0400

    Change this script to double-check a bit more.

--- build-to-war.sh
+++ build-to-war.sh
@@ -3,7 +3,13 @@ if [[ -z "$MESHRDF_HOME" ]]; then
     echo "You must first define $MESHRDF_HOME\n" 1>&2
     exit 1
 fi
-. $MESHRDF_HOME/set_env_mesh
+if [[ ! -d $MESHRDF_HOME/opt/maven/current ]]; then
+    echo "Maven is not installed in $MESHRDF_HOME\n" 1>&2
+fi
+which mvn >& /dev/null
+if [[ $? -ne 0 ]]; then 
+    . $MESHRDF_HOME/set_env_mesh
+fi
 (cd lode-core-api; mvn install)
 (cd lode-core-servlet; mvn install)
 (cd lode-virtuoso-impl; mvn install)
