8eb7fdd4b529b3474d64d17e006b708cd57c4060
Daniel Davis <daniel.davis@nih.gov>
2015-05-18 17:11:57 -0400

    Bootstrap maven even if Maven has not been defined.

--- build-to-war.sh
+++ build-to-war.sh
@@ -1,3 +1,9 @@
+#/bin/bash
+if [[ -z "$MESHRDF_HOME" ]]; then 
+    echo "You must first define $MESHRDF_HOME\n" 1>&2
+    exit 1
+fi
+. $MESHRDF_HOME/set_env_mesh
 (cd lode-core-api; mvn install)
 (cd lode-core-servlet; mvn install)
 (cd lode-virtuoso-impl; mvn install)
