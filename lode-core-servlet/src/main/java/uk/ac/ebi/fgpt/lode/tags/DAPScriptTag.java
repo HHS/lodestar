e7a57f4105bfff23d9637850fc30bc5a191a3dde
Daniel Davis <daniel.davis@nih.gov>
2015-06-10 12:23:20 -0400

    Custom JSP tag to optionally emit script tag for DAP universal analytics
    - Tag currently produces code that shouldn't be useful for anyone, but it tests that the tag itself is working.
    - Plan is to change it to include the analytics script once it has been tested.

--- lode-core-servlet/src/main/java/uk/ac/ebi/fgpt/lode/tags/DAPScriptTag.java
+++ lode-core-servlet/src/main/java/uk/ac/ebi/fgpt/lode/tags/DAPScriptTag.java
@@ -3,32 +3,17 @@ package uk.ac.ebi.fgpt.lode.tags;
 import java.io.IOException;
 import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.JspFragment;    /* unused? */
-import javax.servlet.jsp.tagext.JspTag;         /* unused? */
-import javax.servlet.jsp.tagext.SimpleTag;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 /**
  * DAPScriptTag inserts the appropriate DAP code in each page to register a visit to the page.
  */
-public class DAPScriptTag implements SimpleTag {
-    JspContext jspContext;
-
-    public void doTag() throws IOExecption, JspException {
-        String production = System.getProperty("lodestar.environment");
-        if (production.equals("production")) {
+public class DAPScriptTag extends SimpleTagSupport {
+    public void doTag() throws IOException, JspException {
+        JspContext jspContext = getJspContext();
+        String environment = System.getenv("MESHRDF_ENV");
+        if (null != environment && environment.equals("production")) {
             jspContext.getOut().print("<script src=\"http://ohdear.this.isnt.right/analytics.js\"></script>");
         }
-    }
-
-    public void setParent(JspTag parent) {
-        // DO NOTHING
-    }
-
-    public void setJspContext(JspContext jspContext) {
-        this.jspContext = jspContext;
-    }
-
-    public void setJspBody(JspFragment body) {
-        // DO NOTHING
     }
 }
