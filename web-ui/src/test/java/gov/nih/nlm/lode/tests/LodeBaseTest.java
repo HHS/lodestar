3736e0f62c228b174233df08c3c72e876aba50e5
Dan Davis <daniel.davis@nih.gov>
2015-06-17 17:52:29 -0400

    Beginnings of test suite assure that each page has (beta) in title and h1, and that div class="navi" is visible and enabled.

--- web-ui/src/test/java/gov/nih/nlm/lode/tests/LodeBaseTest.java
+++ web-ui/src/test/java/gov/nih/nlm/lode/tests/LodeBaseTest.java
@@ -1,22 +1,44 @@
 package gov.nih.nlm.lode.tests;
 
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 
 import gov.nih.nlm.occs.selenium.SeleniumTest;
 
 /**
- *  * Having our own base class limits dependence on SeleniumTest.
- *   * We also use it to make sure all tests know how to login.
- *    */
+ * Having our own base class limits dependence on SeleniumTest,
+ * and allows us to find elements after opening a page.
+ */
 public class LodeBaseTest extends SeleniumTest {
-  
-  public HomePage openHomePage() {
-    driver.get(baseUrl + HomePage.URI);
-    return PageFactory.initElements(driver, HomePage.class);
+
+  public static final String OFLAXIN_RELURI = "/D015242";
+  public static final String YEAR_PREFIX = "/2015";
+
+  public String getLodeBaseUrl() {
+    if (baseUrl == null || baseUrl.equals("")) {
+      return "http://iddev.nlm.nih.gov/mesh";
+    } else {
+      return baseUrl;
+    }
   }
 
-  /* public SparqlPage openSparqlPage() {
-    driver.get(baseUrl + SparqlPage.URI);
-    return PageFactory.initElements(driver, SparqlPage.class);
-  }*/ 
+  public void navigationShouldBeValid() {
+    WebElement navi = findElement(By.cssSelector("#home > .navi > ul"));
+    elementShouldBeEnabled(navi);
+  }
+
+  public void openHomePage() {   
+    driver.get(getLodeBaseUrl());
+  }
+ 
+  public void openSparqlPage() {
+    driver.get(getLodeBaseUrl() + "/sparql");
+  }
+
+  public void openExplorerPage(Boolean useprefix) {
+    String prefix = (useprefix ? YEAR_PREFIX : "");
+    String uri = getLodeBaseUrl() + prefix + OFLAXIN_RELURI;
+    driver.get(uri);
+  }
+
 }
